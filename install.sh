987b2e31473db02a4a538b3322b10837085ff03e
dk1302 <deank293@gmail.com>
2025-06-23 15:39:04 +0100

    edited install

--- install.sh
+++ install.sh
@@ -1,34 +1,12 @@
 !#/bin/bash
-sudo apt install xclip cmake libevent ncurses libevent-dev ncurses-dev build-essential bison pkg-config ffmpeg 7zip jq poppler-utils ripgrep libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev stow
-#imagemagick
-git clone --depth 1 --branch [latest_release_tag] https://github.com/ImageMagick/ImageMagick.git ImageMagick-7.1.1
-cd ImageMagick-7.1.1
-./configure
-make
-./configure --with-modules
-sudo make install
-sudo ldconfig /usr/local/lib
 cd ~
-rm -rf ImageMagick-7.1.1
+sudo apt install xclip cmake libevent ncurses libevent-dev ncurses-dev build-essential bison pkg-config ffmpeg 7zip jq poppler-utils ripgrep libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev stow autoconf automake imagemagick tmux
 #fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-cd ~
 #zoxide
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 echo "eval "$(zoxide init --cmd cd bash)"" >> ~/.bashrc
-cd ~
-#tmux
-git clone https://github.com/tmux/tmux.git
-cd tmux
-sh autogen.sh
-./configure
-make && sudo make install
-cd ~
-rm -rf tmux
-#rust
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup update
 #fd
 git clone https://github.com/sharkdp/fd
 cd fd
@@ -45,17 +23,17 @@ mv target/release/yazi target/release/ya /usr/local/bin/
 cd ~
 rm -rf yazi
 #docker
-sudo apt-get update
-sudo apt-get install ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
+sudo apt-get -y update
+sudo apt-get -y install ca-certificates curl
+sudo -y install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get -y update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 #alacritty
 git clone https://github.com/alacritty/alacritty.git
 cd alacritty
@@ -76,10 +54,8 @@ sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo add-apt-repository ppa:openrazer/stable
 sudo add-apt-repository ppa:polychromatic/stable
-sudo apt update
-sudo apt install fastfetch neovim polychromatic
-#dotfiles
-git clone https://github.com/dk1302/dotfiles.git
+sudo apt-get -y update
+sudo apt-get -y install fastfetch neovim polychromatic
 cd dotfiles
 stow .
 
