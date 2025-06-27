83d1aeabfc20a94b379d6d44c8e082e0bc684e9f
dk1302 <deank293@gmail.com>
2025-06-27 20:09:46 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 sudo apt install -y ffmpeg 7zip jq poppler-utils fd-find ripgrep fzf zoxide tmux xclip 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 #ppa's
-sudo add-apt-repository -y ppa:zhangsongcui3371/fastfetch
-sudo add-apt-repository -y ppa:neovim-ppa/unstable
-sudo add-apt-repository -y ppa:openrazer/stable
-sudo add-apt-repository -y ppa:polychromatic/stable
+sudo add-apt-repository ppa:zhangsongcui3371/fastfetch -y
+sudo add-apt-repository ppa:neovim-ppa/unstable -y
+sudo add-apt-repository ppa:openrazer/stable -y
+sudo add-apt-repository ppa:polychromatic/stable -y
 sudo apt update -y
 sudo apt install -y fastfetch neovim polychromatic
 echo "fastfetch -l ~/.ascii" >> ~/.bashrc
