a7f389e63b56e607517ac40722521894e790953d
dk1302 <deank293@gmail.com>
2025-06-27 21:46:32 +0100

    Update .install.sh

--- .install.sh
+++ .install.sh
@@ -1,5 +1,5 @@
 sudo apt install -y cmake g++ pkg-config libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3 \
-                    ffmpeg 7zip jq poppler-utils fd-find ripgrep fzf zoxide tmux xclip 
+                    ffmpeg 7zip jq poppler-utils fd-find ripgrep fzf zoxide tmux xclip dotnet-sdk-9.0
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 #ppa's
