a57fdd9c8863fc38c77c3f83b5595b6e891cf08e
dk1302 <deank293@gmail.com>
2025-07-07 13:57:44 +0100

    network-manager-applet added

--- base.sh
+++ base.sh
@@ -1,3 +1,3 @@
 sudo pacman -S gtk4 gtk3 waybar base-devel freetype2 fontconfig pkg-config make libxcb libxkbcommon python \
                     ffmpeg 7zip jq poppler fd ripgrep tmux wl-clipboard unzip firefox nemo rofi stow blender \
-                    nm-connection-editor blueman bluez python-pywal uwsm libnewt fastfetch swaync
+                    network-manager-applet nm-connection-editor blueman bluez python-pywal uwsm libnewt fastfetch swaync
