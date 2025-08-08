34fcd416ab84697b4aa8c89ae4090d8b6843b0f7
dk1302 <deank293@gmail.com>
2025-08-08 14:07:24 +0100

    added nvim

--- base.sh
+++ base.sh
@@ -1,3 +1,3 @@
-sudo pacman -S gtk4 gtk3 waybar base-devel freetype2 fontconfig pkg-config make libxcb libxkbcommon python \
+sudo pacman -S gtk4 gtk3 waybar base-devel freetype2 fontconfig pkg-config make libxcb libxkbcommon python nvim \
                     ffmpeg 7zip jq poppler fd ripgrep tmux wl-clipboard unzip firefox nemo rofi stow blender \
                     network-manager-applet nm-connection-editor blueman bluez python-pywal uwsm libnewt fastfetch swaync
